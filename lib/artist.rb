d05d6fec9e9c7435d047b68628c9f1959378533d
DANIELLE PARK <dpark1008@gmail.com>
2019-01-16 06:12:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,6 @@
 class Artist
   attr_accessor :name, :songs
 
-  @@count = 0
-
   def initialize(name)
     @name = name
     @songs = []
@@ -11,17 +9,11 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    @@count += 1
   end
 
   def add_song_by_name(song)
     song = Song.new(name)
     @songs << song
     song.artist = self
-    @@count += 1
-  end
-
-  def self.song_count
-    @@count
   end
 end
