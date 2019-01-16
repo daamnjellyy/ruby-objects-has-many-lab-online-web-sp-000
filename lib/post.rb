ebba6994e5fdbf450c45879e8cdff9611c3c8d69
DANIELLE PARK <dpark1008@gmail.com>
2019-01-16 06:18:37 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,15 @@
 class Post
-  attr_accessor :title, :author
-  @@all = 0
 
+  attr_accessor :title, :author
   def initialize(title)
     @title = title
-    @@all << self
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
   end
 end
